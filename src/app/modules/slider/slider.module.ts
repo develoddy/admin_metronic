7c48202335284786086d15a0622168958fa2fe42
Eddy Lujan <eddylujann@gmail.com>
2023-12-09 22:00:47 +0100

    slider module

--- src/app/modules/slider/slider.module.ts
+++ src/app/modules/slider/slider.module.ts
@@ -8,12 +8,27 @@ import { EditNewSliderComponent } from './edit-new-slider/edit-new-slider.compon
 import { DeleteNewSliderComponent } from './delete-new-slider/delete-new-slider.component';
 import { ListSliderComponent } from './list-slider/list-slider.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule, NgbModalModule, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { CRUDTableModule } from 'src/app/_metronic/shared/crud-table';
+import { InlineSVGModule } from 'ng-inline-svg';
+
 
 @NgModule({
   declarations: [SliderComponent, AddNewSliderComponent, EditNewSliderComponent, DeleteNewSliderComponent, ListSliderComponent],
   imports: [
     CommonModule,
-    SliderRoutingModule
+    SliderRoutingModule,
+
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    InlineSVGModule,
+    CRUDTableModule,
+    NgbModalModule,
+    NgbDatepickerModule,
   ]
 })
 export class SliderModule { }
