af12cdf8233817b2cea0851fdf29dd8c1e3868f9
Eddy Lujan <eddylujann@gmail.com>
2023-12-09 20:39:09 +0100

    add modules slider

---
+++ src/app/modules/slider/delete-new-slider/delete-new-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-new-slider',
+  templateUrl: './delete-new-slider.component.html',
+  styleUrls: ['./delete-new-slider.component.scss']
+})
+export class DeleteNewSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
