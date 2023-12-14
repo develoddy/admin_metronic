2724ae5f018945130f2d3c57d8a81591a0a402e1
Eddy Lujan <eddylujann@gmail.com>
2023-12-15 00:06:13 +0100

    edit cupon

--- src/app/modules/cupone/list-cupones/list-cupones.component.ts
+++ src/app/modules/cupone/list-cupones/list-cupones.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CuponeService } from '../_services/cupone.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-cupones',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListCuponesComponent implements OnInit {
 
-  constructor() { }
+  cupones:any=[];
+  search:any = "";
+  isLoading$:any = null;
+
+  constructor(
+    public _serviceCupon: CuponeService,
+    public _modalService: NgbModal,
+    public _router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.isLoading$ = this._serviceCupon.isLoading$;
+    this.allCupones();
   }
 
+  refresh() {
+    this.search = "";
+    this.allCupones();
+  }
+
+  allCupones() {
+    this._serviceCupon.allCupones(this.search).subscribe((resp:any) => {
+      this.cupones = resp.cupones;
+    });
+  }
+
+  editCupon(cupon){
+    this._router.navigateByUrl("/cupones/edit-cupon/"+cupon._id);
+  }
+  delete(cupon){
+
+  }
 }
